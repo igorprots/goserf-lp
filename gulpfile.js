035f4eef5f100303defccac0f026864ef4eba84b
Igor Protsenko <protsxe51@gmail.com>
2020-09-18 13:39:02 +0300

    fix animate

--- gulpfile.js
+++ gulpfile.js
@@ -13,13 +13,13 @@ gulp.task('clean', async function () {
 })
 
 gulp.task('scss', function () {
-  return gulp.src('app/scss/**/*.scss')
+  return gulp.src('docs/scss/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 8 versions']
     }))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('app/css'))
+    .pipe(gulp.dest('docs/css'))
     .pipe(browserSync.reload({ stream: true }))
 });
 
@@ -30,17 +30,17 @@ gulp.task('css', function () {
     'node_modules/animate.css/animate.compat.css',
   ])
     .pipe(concat('_libs.scss'))
-    .pipe(gulp.dest('app/scss'))
+    .pipe(gulp.dest('docs/scss'))
     .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('html', function () {
-  return gulp.src('app/*.html')
+  return gulp.src('docs/*.html')
     .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('script', function () {
-  return gulp.src('app/js/*.js')
+  return gulp.src('docs/js/*.js')
     .pipe(browserSync.reload({ stream: true }))
 });
 
@@ -50,39 +50,39 @@ gulp.task('js', function () {
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('app/js'))
+    .pipe(gulp.dest('docs/js'))
     .pipe(browserSync.reload({ stream: true }))
 });
 
 gulp.task('browser-sync', function () {
   browserSync.init({
     server: {
-      baseDir: "app/"
+      baseDir: "docs/"
     }
   });
 });
 
 gulp.task('export', function () {
-  let buildHtml = gulp.src('app/**/*.html')
+  let buildHtml = gulp.src('docs/**/*.html')
     .pipe(gulp.dest('dist'));
 
-  let BuildCss = gulp.src('app/css/**/*.css')
+  let BuildCss = gulp.src('docs/css/**/*.css')
     .pipe(gulp.dest('dist/css'));
 
-  let BuildJs = gulp.src('app/js/**/*.js')
+  let BuildJs = gulp.src('docs/js/**/*.js')
     .pipe(gulp.dest('dist/js'));
 
-  let BuildFonts = gulp.src('app/fonts/**/*.*')
+  let BuildFonts = gulp.src('docs/fonts/**/*.*')
     .pipe(gulp.dest('dist/fonts'));
 
-  let BuildImg = gulp.src('app/img/**/*.*')
+  let BuildImg = gulp.src('docs/img/**/*.*')
     .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
-  gulp.watch('app/*.html', gulp.parallel('html'))
-  gulp.watch('app/js/*.js', gulp.parallel('script'))
+  gulp.watch('docs/scss/**/*.scss', gulp.parallel('scss'));
+  gulp.watch('docs/*.html', gulp.parallel('html'))
+  gulp.watch('docs/js/*.js', gulp.parallel('script'))
 });
 
 gulp.task('build', gulp.series('clean', 'export'))
