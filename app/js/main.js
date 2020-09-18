15a7b09dcbe4796a016f9df26285620f789717d4
Igor Protsenko <protsxe51@gmail.com>
2020-09-18 18:37:04 +0300

    add new slider

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,13 @@ $(function () {
         slidesToScroll: 1,
         asNavFor: '.header__slider',
     });
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+
+    });
 
 
 });
