302fc858e07b077c38084d7978268955e1309099
Igor Protsenko <protsxe51@gmail.com>
2020-09-21 19:14:16 +0300

    add new block travel

--- app/js/main.js
+++ app/js/main.js
@@ -28,6 +28,12 @@ $(function () {
         asNavFor: '.surf-slider',
         focusOnSelect: true
     })
+    $('.travel__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
+    })
 
 
 });
