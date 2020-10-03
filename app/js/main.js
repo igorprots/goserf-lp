347cdc4bb7740f13afb59d3b2236be0c3c87a15a
Igor Protsenko <protsxe51@gmail.com>
2020-10-03 20:58:53 +0300

    start adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -8,33 +8,105 @@ $(function () {
         asNavFor: '.slider-dotshead',
 
     });
+
     $('.slider-dotshead').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
         asNavFor: '.header__slider',
+        responsive: [
+            {
+                breakpoint: 961,
+                settings: "unslick"
+            },
+        ]
     });
+
     $('.surf-slider').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
-        asNavFor: '.slider-map'
+        asNavFor: '.slider-map',
+        responsive: [
+            {
+                breakpoint: 1210,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+
+                }
+            },
+            {
+                breakpoint: 720,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true
+                }
+            },
+            {
+                breakpoint: 426,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: false
+                }
+            }
+        ]
 
     });
+
     $('.slider-map').slick({
         slidesToShow: 8,
         slidesToScroll: 1,
         arrows: false,
         asNavFor: '.surf-slider',
-        focusOnSelect: true
-    })
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 1103,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 900,
+                settings: {
+                    slidesToShow: 2,
+                    centerMode: true
+                }
+            },
+            {
+                breakpoint: 720,
+                settings: {
+                    slidesToShow: 1,
+                    centerMode: true
+                }
+            }
+        ]
+    });
+
     $('.holder__slider, .shop__slider').slick({
         infinite: true,
         fade: true,
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrows-left.svg" alt=""></img>',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrows-right.svg" alt=""></img>',
-    })
+    });
 
+
+    $('.surfboard-box__circle').on('click', function () {
+        $(this).toggleClass('active');
+    });
+
+    $('.menu-btn').on('click', function () {
+        $('#menu').toggleClass('active');
+    });
 
 });
 
@@ -75,17 +147,9 @@ $('.quantity').each(function () {
         $('.summ').html('$' + summ);
     });
 
-    // let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests')
+    let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests')
 
-    // $('.summ').html('$' + summ);
+    $('.summ').html('$' + summ);
 
-
-    $('.surfboard-box__circle').on('click', function () {
-        $(this).toggleClass('active');
-    });
-
-    $('.menu-btn').on('click', function () {
-        $('#menu').toggleClass('active')
-    })
 
 });
